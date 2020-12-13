02feb7c74604a75ad1982ee7cc2ccc5415cec04c
Bryan Koen <bryan.koen@gmail.com>
2020-12-12 23:45:46 -0600

    Completed section 3&4

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,23 +1,21 @@
 // tests/health_check.rs
 
+use std::net::TcpListener;
+
 // `actix_rt::test` is the testing equivalent of `actix_rt::main`.
 // It also spares you from having to specify the `#[test]` attribute.
 // You can inspect what code gets generated using
 // `cargo expand --test health_check` (<- name of the test file)
 #[actix_rt::test]
 async fn health_check_works() {
-    // No .await, no .expect
-    spawn_app();
-
-    // We brought `reqwest` in as a _development_ dependency
-    // to perform HTTP requests against our application.
-    // Either add it manually under [dev-dependencies] in Cargo.toml
-    // or run `cargo add reqwest --dev`
+    // Arrange
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        // Use the returned application address
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -31,12 +29,12 @@ async fn health_check_works() {
 // We are also running tests, so it is not worth it to propagate errors:
 // if we fail to perform the required setup we can just panic and crash
 // all the things.
-fn spawn_app() {
-    // New dev dependency - let's add tokio to the party with
-    // `cargo add tokio --dev --vers 0.2.22`
-    let server = zero2prod::run().expect("Failed to bind address");
-    // Launch the server as a background task
-    // tokio::spawn returns a handle to the spawned future,
-    // but we have no use for it here, hence the non-binding let
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // We retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
+    // We return the application address to the caller!
+    format!("http://127.0.0.1:{}", port)
 }
